5cc9fa13814f2bc3797d79ddb77c1819837de5b4
20120155 <83502703+Foam2702@users.noreply.github.com>
2023-04-25 18:21:06 +0700

    add carousel

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
-const express=require('express')
-const exphbs=require('express-handlebars')
-const port=3000;
-const app=express();
+const express = require('express')
+const exphbs = require('express-handlebars')
+const port = 3000;
+const app = express();
 
 /* must */
 var bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use("/public", express.static("public"));
 
-app.engine('hbs',exphbs.engine({
-    layoutsDir:'./views/_layouts',
-    defaultLayout:'main.hbs',
-    partialsDir:'./views/_partials',
-    extname:'hbs'
+app.engine('hbs', exphbs.engine({
+    layoutsDir: './views/_layouts',
+    defaultLayout: 'main.hbs',
+    partialsDir: './views/_partials',
+    extname: 'hbs'
 }))
 
 
-app.set('view engine','hbs')
 
-app.get('/',function(req,res){
+app.set('view engine', 'hbs')
+
+app.get('/', function (req, res) {
     res.render('home')
 })
 
-const blockRouter=require('./routes/blocks.r');
-app.use('/',blockRouter)
-const convertRouter=require('./routes/convertmoney.r');
-app.use('/',convertRouter)
-const loginRouter=require('./routes/login.r');
-app.use('/',loginRouter)
+const blockRouter = require('./routes/blocks.r');
+app.use('/', blockRouter)
+const convertRouter = require('./routes/convertmoney.r');
+app.use('/', convertRouter)
+const loginRouter = require('./routes/login.r');
+app.use('/', loginRouter)
 
-app.listen(port,function(){
+app.listen(port, function () {
     console.log(`Server is running at http://localhost:${port}`);
 })
