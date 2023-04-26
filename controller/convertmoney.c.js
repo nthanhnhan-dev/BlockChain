3746b530f7e69de9aa72377119c757e2dd9b4212
20120155 <83502703+Foam2702@users.noreply.github.com>
2023-04-26 19:39:15 +0700

    add pending

--- controller/convertmoney.c.js
+++ controller/convertmoney.c.js
@@ -1,10 +1,26 @@
 const moneyM = require("../model/convertmoney.m")
 const userM = require("../model/login.m")
+
+
+const hash = require('crypto-js/sha256');
+const he = require('he');
+
+exports.pending = async (req, res, next) => {
+    if (req.method == "GET") {
+        res.render("convertmoney/pending", {
+
+        })
+    }
+    else if (req.method == "POST") {
+    }
+}
+
+
 exports.sendmoney = async (req, res, next) => {
     if (req.method == "GET") {
         const user = await userM.getUserByName(req.session.user);
         const alluser = await userM.getAllUserExceptOwner(req.session.user);
-        res.render('./convertmoney/sendmoney',
+        res.render('convertmoney/sendmoney',
             {
                 account: req.session.user,
                 user: user[0],
--- routes/convertmoney.r.js
+++ routes/convertmoney.r.js
@@ -1,13 +1,14 @@
-const app=require('express')
-const router=app.Router()
-const BlockC=require('../controller/convertmoney.c')
+const app = require('express')
+const router = app.Router()
+const BlockC = require('../controller/convertmoney.c')
 
 
-router.get('/sendmoney',BlockC.sendmoney)
+router.get('/sendmoney', BlockC.sendmoney)
 
 
-router.post('/sendmoney',BlockC.sendmoney)
+router.post('/sendmoney', BlockC.sendmoney)
+router.get('/pending', BlockC.pending)
+router.post('/pending', BlockC.pending)
 
 
-
-module.exports=router;
+module.exports = router;
