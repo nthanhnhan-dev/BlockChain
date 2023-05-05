300dcfcf6d79745ebeddb96e95a8776c3103a4e1
unknown <ADMIN@DESKTOP-959V0MH.VIETPHAM>
2023-05-05 17:16:28 +0700

    if nothing new,dont mining

--- controller/blocks.c.js
+++ controller/blocks.c.js
@@ -119,6 +119,7 @@ exports.createBlockChain = async (req, res, next) => {
     var transactions_to_blockchain = []
     var transactions_to_block;
     var preHash;
+    var check = 0;
     var block = []
     for (var i = 0; i < transactions.length; i++) {
         var exists = await blockM.checkExistTransationInBlock(transactions[i].ID_TRANSACTION);
@@ -133,6 +134,7 @@ exports.createBlockChain = async (req, res, next) => {
             }
             //transactions_to_blockchain.push(transactions_to_block);
             await blockM.addBlock(transactions_to_block)
+            check++;
         }
         else {
 
@@ -141,6 +143,20 @@ exports.createBlockChain = async (req, res, next) => {
         }
 
     }
-    res.redirect("/blockchain")
+    if (check != 0) {
+        res.redirect("/blockchain")
+
+    }
+    else {
+        const transaction_BD = await moneyM.getAllTransactions();
+
+        res.render("convertmoney/pending", {
+            transaction: transaction_BD,
+
+            account: req.session.user,
+            announce: "NOTTHING NEW TO MINING"
+        })
+
+    }
 
 }
